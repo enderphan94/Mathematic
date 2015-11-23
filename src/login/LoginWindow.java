498897c3197d90a33c0df56b76d408b9bc24fe77
enderphan94 <weintome@gmail.com>
2015-11-23 17:43:36 +0200

    Key ENter

--- src/login/LoginWindow.java
+++ src/login/LoginWindow.java
@@ -8,6 +8,9 @@ import java.awt.Graphics;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -29,18 +32,16 @@ import javax.swing.JTextField;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
-
-
-public class LoginWindow extends JFrame{
+public class LoginWindow extends JFrame {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	public LoginWindow() {
 		super("Login");
 		setSize(300, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel panel = new JPanel(){
+
+		JPanel panel = new JPanel() {
 			public void paintComponent(Graphics g) {
 				BufferedImage img = null;
 				System.out.println("paint");
@@ -50,115 +51,59 @@ public class LoginWindow extends JFrame{
 					e.printStackTrace();
 				}
 				g.drawImage(img, 0, 0, null);
-				//super.paint(g);
+				// super.paint(g);
 			}
 		};
 		add(panel);
 		placeComponents(panel);
-		
+
 		setVisible(true);
 		setLocationRelativeTo(null);
 	}
-	
-	
+
+	JLabel userLabel = new JLabel("Email         :");
+	JTextField userText = new JTextField(20);
+	JLabel passwordLabel = new JLabel("Password :");
+	JPasswordField passwordText = new JPasswordField(20);
+	JButton loginButton = new JButton("login");
 
 	private void placeComponents(JPanel panel) {
 		panel.setLayout(null);
-		JLabel userLabel = new JLabel("Email         :");
 		userLabel.setBounds(10, 10, 80, 25);
 		panel.add(userLabel);
 
-		JTextField userText = new JTextField(20);
 		userText.setBounds(100, 10, 160, 25);
 		panel.add(userText);
 
-		JLabel passwordLabel = new JLabel("Password :");
 		passwordLabel.setBounds(10, 40, 80, 25);
 		panel.add(passwordLabel);
 
-		JPasswordField passwordText = new JPasswordField(20);
+		KeyListener enterLogin = new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (KeyEvent.VK_ENTER == e.getKeyCode()) {
+					doLogin();
+				}
+			}
+		};
+
 		passwordText.setBounds(100, 40, 160, 25);
+		passwordText.addKeyListener(enterLogin);
 		panel.add(passwordText);
 
-		JButton loginButton = new JButton("login");
 		loginButton.setBounds(30, 80, 80, 25);
+		loginButton.addKeyListener(enterLogin);
 		panel.add(loginButton);
-		
-		
 
 		// login
 		loginButton.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
-				
-				String usern = userText.getText();
 
-				if (usern.trim().isEmpty()) {
-
-					JOptionPane.showMessageDialog(null, "Please Enter Username");
-
-					userText.selectAll();
-
-					userText.requestFocus();
-
-					return;
-
-				}
-				//Using Regular Expression 
-				 StringBuilder regex = new StringBuilder();
-			        regex.append("^");
-			        regex.append("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
-			        regex.append("$");
-			        Pattern pattern = Pattern.compile(regex.toString());
-			        Matcher matcher = pattern.matcher(usern);
-			        boolean valid = matcher.matches();
-			        if (valid) {
-			        	JOptionPane.showMessageDialog(null, "You have loged in successfully");
-			        } else {
-			        	JOptionPane.showMessageDialog(null, "Email is invalid");
-			        	return;
-			        }
-			    //Password check
-			    
-				@SuppressWarnings("deprecation")
-				String passw = passwordText.getText();
-				String hash = DigestUtils.shaHex(passw);
-				if (passw.trim().isEmpty()) {
-
-					JOptionPane.showMessageDialog(null, "Please Enter Password");
-
-					passwordText.selectAll();
-
-					passwordText.requestFocus();
- 
-					return;
-
-				}
-				
-			//Get User Information
-				try {
-					  File out = new File("/users/Ender/Desktop/GetUser/InputUser.txt");
-					 if (!out.exists()) {
-		                    out.createNewFile();
-		                }
-					
-					  FileWriter fw = new FileWriter(out.getAbsoluteFile());
-		                BufferedWriter bw = new BufferedWriter(fw);
-		                bw.write(usern+ "\t" + hash);    
-		                bw.close();
-
-		                System.out.println("Done");
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				ChooseWindow ptb = new ChooseWindow();
-				ptb.setVisible(true);
-				LoginWindow.this.dispose();
+				doLogin();
 			}
 		});
-		//EXIT
+		// EXIT
 		JButton btnregis = new JButton("Sign up");
 		btnregis.setBounds(150, 80, 100, 25);
 		panel.add(btnregis);
@@ -171,6 +116,73 @@ public class LoginWindow extends JFrame{
 
 			}
 		});
+	}
+
+	private void doLogin() {
+		String usern = userText.getText();
+
+		if (usern.trim().isEmpty()) {
+
+			JOptionPane.showMessageDialog(null, "Please Enter Username");
+
+			userText.selectAll();
+
+			userText.requestFocus();
+
+			return;
+
+		}
+		// Using Regular Expression
+		StringBuilder regex = new StringBuilder();
+		regex.append("^");
+		regex.append("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+		regex.append("$");
+		Pattern pattern = Pattern.compile(regex.toString());
+		Matcher matcher = pattern.matcher(usern);
+		boolean valid = matcher.matches();
+		if (valid) {
+			JOptionPane.showMessageDialog(null, "You have loged in successfully");
+		} else {
+			JOptionPane.showMessageDialog(null, "Email is invalid");
+			return;
+		}
+		// Password check
+
+		@SuppressWarnings("deprecation")
+		String passw = passwordText.getText();
+		String hash = DigestUtils.shaHex(passw);
+		if (passw.trim().isEmpty()) {
+
+			JOptionPane.showMessageDialog(null, "Please Enter Password");
+
+			passwordText.selectAll();
+
+			passwordText.requestFocus();
+
+			return;
+
+		}
+
+		// Get User Information
+		try {
+			File out = new File("/users/Ender/Desktop/GetUser/InputUser.txt");
+			if (!out.exists()) {
+				out.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(out.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(usern + "\t" + hash);
+			bw.close();
+
+			System.out.println("Done");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ChooseWindow ptb = new ChooseWindow();
+		ptb.setVisible(true);
+		LoginWindow.this.dispose();
 	}
 
 }
